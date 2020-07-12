20ba0a0c5fa6aab9f14575c56a57746a08c1136a
Bartek <popiolekbartlomiej368@gmail.com>
2020-07-12 22:01:35 +0200

    add .js file

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".contact-form");
+const inputForm = form["text-input"];
+// const textInput = document.getElementById("text-input");
+const spanValidEmail = document.querySelector(".valid-email");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+inputForm.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  let inputFormIncludes =
+    inputForm.value.includes("@") && inputForm.value.includes(".");
+  let inputFormInclude = inputForm.value.indexOf("@.");
+  let inputFormLength = inputForm.value.length;
+  let inputFormIndexOf = inputForm.value.indexOf(".") + 1;
+
+  if (
+    (inputFormIncludes &&
+      inputFormIndexOf !== inputFormLength &&
+      inputFormInclude === -1) ||
+    inputFormLength === 0
+  ) {
+    spanValidEmail.style.display = "none";
+    inputForm.classList.remove("text-input-active");
+  } else {
+    spanValidEmail.style.display = "block";
+    inputForm.classList.add("text-input-active");
+  }
+});
